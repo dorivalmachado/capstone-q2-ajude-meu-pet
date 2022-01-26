60b8f4578cfd94540641d07f6637a4cf65b440de
Sarah Scardini <sarahscardini@gmail.com>
2022-01-26 20:39:54 -0300

    feat(input): create input component

---
+++ src/Components/Input/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  input:placeholder-shown {
+    font-style: italic;
+    font-family: inherit;
+  }
+
+  input:-webkit-autofill {
+    border: 1px solid white;
+    box-shadow: 0 0 0px 1000px transparent inset;
+    font-family: inherit;
+    transition: background-color 5000s ease-in-out 0s;
+    -webkit-text-fill-color: black;
+    -webkit-box-shadow: 0 0 0px 1000px transparent inset;
+  }
+`;
