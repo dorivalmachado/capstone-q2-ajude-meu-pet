85201c14d28d6b174c25190e22e185e77c78e279
Rafael Monteiro de Oliveira <rafael.monteiro.oliveira@gmail.com>
2022-01-28 16:17:13 -0300

    fix: providers, register/login page and confirmPass
    login/register page: put components into routes (and linking) to test register/login
    providers
    
    providers: deleted non necessary providers structures (register/login),
    and fixed wrong load from pets/services when not logged in
    
    deleted passwordConf from send to endpoint when registering a new user

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -3,18 +3,19 @@ import LandingPage from "../Pages/LandingPage";
 import { useAuth } from "../Providers/Auth";
 import Route from "./Route";
 import { Login } from "../Pages/Login";
+import { Register } from "../Components/Register";
 
 const Routes = () => {
   const { token } = useAuth();
 
   return (
     <Switch>
-      {/* <Route exact path="/" component={LandingPage} />
+      <Route exact path="/" component={LandingPage} />
       <Route exact path="/login" component={Login} />
-      <Route exact path="/signup" component={() => {}} />
-      <Route exact path="/services" isPrivate component={() => {}} />
-      <Route exact path="/pets" isPrivate component={() => {}} />
-      <Route component={() => {}} isPrivate={!!token} /> */}
+      <Route exact path="/register" component={Register} />
+      {/* <Route exact path="/services" isPrivate component={() => {}} />
+      {/* <Route exact path="/pets" isPrivate component={() => {}} /> */}
+      {/* <Route component={() => {}} isPrivate={!!token} /> */}
     </Switch>
   );
 };
