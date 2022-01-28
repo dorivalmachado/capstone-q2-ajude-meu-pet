97693141a7c36042d90d804d46ae9542d261c0e9
Lucas <lcssreiser@gmail.com>
2022-01-28 14:25:36 -0300

    bug: fixing a bug in perfil

---
+++ src/Components/Profile/index.jsx
@@ -0,0 +1,12 @@
+import { Container, Content } from "./styles";
+
+const Profile = () => {
+  return (
+    // <Container>
+    //   <Content>Perfil</Content>
+    // </Container>
+    <div>perfil</div>
+  );
+};
+
+export default Profile;
---
+++ src/Components/Profile/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  width: 90vw;
+  height: 80vh;
+  background-color: #ad6d53;
+`;
