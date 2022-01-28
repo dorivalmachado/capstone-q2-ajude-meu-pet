638ff5132a02d96c48b2c95d4d0055621d47dfdf
Gustavo <gu.machado.oliveira@gmail>
2022-01-28 11:48:07 -0300

    feat(headerlogged): creation of the side bar menu when user logged

---
+++ src/Components/HeaderLogged/index.jsx
@@ -0,0 +1,31 @@
+import { Container, MainPage, Logo, Menu } from "./styles";
+import { GoSignOut } from "react-icons/go";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../Providers/Auth";
+
+export const HeaderLogged = () => {
+  const { logout } = useAuth();
+  return (
+    <Container>
+      <Logo />
+      <Menu>
+        <div>
+          <Link to="/pets">
+            <p>Meus pets</p>
+          </Link>
+        </div>
+        <div>
+          <Link to="">
+            <p>Serviços</p>
+          </Link>
+        </div>
+        <div>
+          <Link to="">
+            <p>Perfil</p>
+          </Link>
+        </div>
+        <GoSignOut size={40} cursor="pointer" onClick={() => logout} />
+      </Menu>
+    </Container>
+  );
+};
