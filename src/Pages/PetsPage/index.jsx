4d908173cb5a7e6834ea0f4b48799ed7f6c68e37
Dorival Machado <dorivalmachado@outlook.com>
2022-01-31 20:18:51 -0300

    refactor(pets providers and its dependent components): rewrite provider to get all pets
    consequently, rewrite all components that render only user's pets

--- src/Pages/PetsPage/index.jsx
+++ src/Pages/PetsPage/index.jsx
@@ -6,23 +6,32 @@ import CardPets from "../../Components/CardPets";
 import CatBox from "../../Assets/Img/catInBox.gif";
 import Header from "../../Components/Header";
 import MainContainer from "../../Components/MainContainer";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ModalAddPet from "../../Components/ModalAddPet";
 import { LightTip } from "../../Helpers/Tooltip";
 import ModalEditPet from "../../Components/ModalEditPet";
+import { useAuth } from "../../Providers/Auth";
 
 
 
 const PetsPage = () => {
   const { pets } = usePets();
+  const {user} = useAuth();
 
   const [openModal, setOpenModal] = useState('open');
   const [petId, setPetId] = useState(0);
+  const [myPets, setMyPets] = useState([]);
 
   const editPet = (id) => {
     setOpenModal('edit');
     setPetId(id);
   }
+
+  useEffect(() => {
+    if(pets.length > 0){
+      setMyPets(pets.filter(pet => pet.userId === user.id))
+    }
+  }, [pets])
 
   return (
     <MainContainer>
@@ -50,9 +59,9 @@ const PetsPage = () => {
             </LightTip>
           </div>
 
-          {pets.length > 0 ? (
+          {myPets.length > 0 ? (
             <PetsBox>
-              {pets.map((pet) => (
+              {myPets.map((pet) => (
                 <CardPets
                   key={pet.id}
                   name={pet.petName}
