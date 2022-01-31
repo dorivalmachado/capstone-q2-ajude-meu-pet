39859bf7450bf69dcc178dd303c74e8b551d2692
Sarah Scardini <sarahscardini@gmail.com>
2022-01-31 09:09:45 -0300

    style(login): add image to login page

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,15 +1,15 @@
 import { Container, FormContainer, ContentContainer } from "./styles";
 import { InputAdornment } from "@mui/material";
 
+import React, { useState } from "react";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useAuth } from "../../Providers/Auth";
 import { Link } from "react-router-dom";
 import { FiEyeOff, FiEye, FiLock, FiUser } from "react-icons/fi";
-import React, { useState } from "react";
 import Button from "../../Components/Button";
-import MainContainer from "../../Components/MainContainer";
+import DogImage from "../../Assets/Img/corgiRunning.gif";
 import Input from "../../Components/Input";
 
 export const Login = () => {
@@ -33,67 +33,66 @@ export const Login = () => {
   });
 
   const handleSignIn = (data) => {
-    // console.log(data);
     login(data);
   };
 
   return (
-    <MainContainer>
-      <Container>
-        <ContentContainer>
-          <FormContainer>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit(handleSignIn)}>
-              <Input
-                label="Email"
-                name="email"
-                type="email"
-                fullWidth
-                register={register}
-                error={!!errors.email}
-                helperText={errors.email?.message}
-                inputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <FiUser />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Input
-                type={showPass ? "text" : "password"}
-                label="Senha"
-                fullWidth
-                register={register}
-                name="password"
-                error={!!errors.password}
-                helperText={errors.password?.message}
-                inputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <FiLock />
-                    </InputAdornment>
-                  ),
-                  endAdornment: (
-                    <InputAdornment
-                      position="end"
-                      onClick={() => setShowPass(!showPass)}
-                    >
-                      {showPass ? <FiEye /> : <FiEyeOff />}
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Button type="submit" buttonColor="lightBrown">
-                Entrar
-              </Button>
-            </form>
-            <p>
-              Não é cadastrado ainda? <Link to="/register">Cadastre-se!</Link>
-            </p>
-          </FormContainer>
-        </ContentContainer>
-      </Container>
-    </MainContainer>
+    <Container>
+      <img src={DogImage} alt="Dog running" />
+
+      <ContentContainer>
+        <FormContainer>
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit(handleSignIn)}>
+            <Input
+              label="Email"
+              name="email"
+              type="email"
+              fullWidth
+              register={register}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              inputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <FiUser />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Input
+              type={showPass ? "text" : "password"}
+              label="Senha"
+              fullWidth
+              register={register}
+              name="password"
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              inputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <FiLock />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment
+                    position="end"
+                    onClick={() => setShowPass(!showPass)}
+                  >
+                    {showPass ? <FiEye /> : <FiEyeOff />}
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Button type="submit" buttonColor="lightBrown">
+              Entrar
+            </Button>
+          </form>
+          <p>
+            Não é cadastrado ainda? <Link to="/register">Cadastre-se!</Link>
+          </p>
+        </FormContainer>
+      </ContentContainer>
+    </Container>
   );
 };
